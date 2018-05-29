cbc3976056ada1b0f195f5f3216cb12312be2ed3
SolenneD <sdaguerre75@gmail.com>
2018-05-29 12:44:55 +0200

    burgermenu + geo et mail fonctionnels

--- sources/ARES/src/app/app.component.ts
+++ sources/ARES/src/app/app.component.ts
@@ -3,31 +3,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { TranslateService } from '@ngx-translate/core';
 import { Config, Nav, Platform } from 'ionic-angular';
-import { GeoPage } from "../pages/geo/geo";
-import { SatisfactionPage } from "../pages/satisfaction/satisfaction";
-
+import { GeoPage } from '../pages/geo/geo';
+import { SatisfactionPage } from '../pages/satisfaction/satisfaction';
 
 import { FirstRunPage } from '../pages';
 import { Settings } from '../providers';
 
 @Component({
-  template: `<ion-menu [content]="content">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Pages</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content>
-      <ion-list>
-        <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">
-          {{p.title}}
-        </button>
-      </ion-list>
-    </ion-content>
-
-  </ion-menu>
-  <ion-nav #content [root]="rootPage"></ion-nav>`
+  templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage = FirstRunPage;
@@ -48,7 +31,7 @@ export class MyApp {
     { title: 'Search', component: 'SearchPage' },
     { title: 'Géolocalisation', component: 'GeoPage'},
     { title: 'Satisfaction', component: 'SatisfactionPage'},
-  ]
+  ];
 
   constructor(private translate: TranslateService, platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
     platform.ready().then(() => {
