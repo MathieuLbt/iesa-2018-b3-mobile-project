02cf7f4dc6b3f7274575fb4cf69957075c7b3abc
SolenneD <sdaguerre75@gmail.com>
2018-05-30 21:47:22 +0200

    camera v2

--- sources/ARES/src/pages/camera/camera.ts
+++ sources/ARES/src/pages/camera/camera.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+
 
 
 /**
@@ -16,11 +17,15 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
   templateUrl: 'camera.html',
 })
 export class CameraPage {
+  public myphotos : any;
+  public base64Image : string;
 
-  myphoto:any;
+  ngOnInit(){
+    this.myphotos = [];
+  }
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera, private alertCtrl: AlertController) {
   }
 
   takePhoto(){
@@ -29,16 +34,43 @@ export class CameraPage {
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
-    }
+    };
 
     this.camera.getPicture(options).then((imageData) => {
+
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
-      this.myphoto = 'data:image/jpeg;base64,' + imageData;
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.myphotos.push(this.base64Image);
+      this.myphotos.reverse();
     }, (err) => {
       // Handle error
     });
   }
+
+  removePhoto(index){
+    let confirm = this.alertCtrl.create({
+      title: 'Vous êtes sur de vouloir supprimer cette photo ?',
+      message: '',
+      buttons: [
+        {
+          text: 'Non',
+          handler: () => {
+
+          }
+        },
+        {
+          text: 'Oui',
+          handler: () => {
+            this.myphotos.splice(index,1);
+          }
+        }
+      ]
+    });
+    confirm.present();
+
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CameraPage');
